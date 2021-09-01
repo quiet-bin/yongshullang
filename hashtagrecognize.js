89ce4cccb1168361a2600ae60cbd0b87c5b49572
quiet-bin <88278327+quiet-bin@users.noreply.github.com>
2021-09-01 22:38:17 +0900

    Add files via upload

--- hashtagrecognize.js
+++ hashtagrecognize.js
@@ -1,72 +1,218 @@
 
-  var 액시트=["한국","코미디","재난","스릴러","액션","가족"];
-  var 날씨의아이=["애니","멜로","뮤직"];
-  var 스타이즈본=["뮤직","멜로","외국"];
-  var 주만지=["판타지","코미디","가족","하이틴"];
-  var 라라랜드=["음악","멜로","드라마","교훈"];
-  var 위대한쇼맨=["음악","멜로","드라마","교훈","가족"];
-  var 범죄도시=["범죄","코미디","액션","한국","느와르"];
-  var 인셉션=["sf","판타지","액션","철학","교훈","반전"];
-  var 인터스텔라=["sf","판타지","철학","교훈","가족"];
-  var 기생충=["한국","철학","교훈","가족","코미디","스릴러"];
-  var 오백일의써머=["멜로","드라마","교훈","음악"];
-  var 어바웃타임=["멜로","드라마","판타지","가족"];
-  var 내부자들=["한국","범죄","느와르","교훈","철학"];
-  var 신세계=["느와르","한국","범죄","액션"];
-  var 해리포터=["시리즈","판타지","하이틴","가족","액션"];
-  var 아이언맨=["마블","액션","판타지","sf","가족","코미디"];
-  var 캡틴아메리카=["마블","액션","판타지","sf","가족","코미디"];
-  var 어벤져스=["마블","액션","판타지","sf","가족","코미디"];
-  var 토르=["마블","액션","판타지","sf","가족","코미디"];
-  var 닥터스트레인지=["마블","액션","판타지","sf","가족","코미디"];
-  var 메이즈러너=["액션","스릴러","시리즈","판타지"];
-  var 랑종=["공포","다큐","철학","교훈"];
-  var 버드박스=["공포","스릴러","철학","액션"];
-  var 칠번방의선물=["한국","가족","드라마","코미디","교훈"];
-  var 세얼간이=["코미디","교훈","철학","드라마","음악"];
-  var 아수라=["한국","액션","범죄","느와르"];
-  var 범죄와의전쟁=["역사","한국","범죄","느와르","액션","코미디","교훈"];
+  var 액시트=['액시트.png',"코미디","재난","액션","가족","B급무비"];
+  var 날씨의아이=["애니","감동","판타지","가족"];
+  var 스타이즈본=["뮤직","멜로","드라마","새드앤딩","청춘","감성"];
+  var 주만지=["판타지","코미디","가족","하이틴","액션","모험"];
+  var 라라랜드=['라라랜드.png',"음악","멜로","드라마","뮤지컬","새드앤딩","감성"];
+  var 위대한쇼맨=["음악","드라마","교훈","가족","뮤지컬","감성"];
+  var 범죄도시=["한국","범죄","코미디","액션","느와르"];
+  var 인셉션=["sf","판타지","액션","미스테리","메세지","반전"];
+  var 인터스텔라=["sf","판타지","미스테리","메세지","가족","반전"];
+  var 기생충=["블랙코미디","메세지","가족","코미디","스릴러","반전"];
+  var 오백일의써머=["멜로","드라마","교훈","새드앤딩","코미디","감성"];
+  var 어바웃타임=["멜로","드라마","판타지","가족","교훈","메세지"];
+  var 내부자들=["다큐","범죄","느와르","교훈","액션","메세지"];
+  var 신세계=["느와르","범죄","액션","반전","메세지"];
+  var 해리포터=["시리즈","판타지","하이틴","판타지","모험","블록버스터"];
+  var 아이언맨=["마블","액션","판타지","sf","블록버스터","코미디"];
+  var 캡틴아메리카=["마블","액션","판타지","sf","블록버스터","코미디"];
+  var 어벤져스=["마블","액션","판타지","sf","블록버스터","코미디"];
+  var 토르=["마블","액션","판타지","sf","블록버스터","코미디"];
+  var 닥터스트레인지=["마블","액션","판타지","sf","블록버스터","코미디"];
+  var 메이즈러너=["액션","스릴러","시리즈","판타지","서스팬스"];
+  var 랑종=["공포","미스테리","철학","서스팬스","메세지","스릴러"];
+  var 버드박스=["공포","스릴러","철학","액션","서스팬스","괴물"];
+  var 칠번방의선물=["한국","가족","드라마","코미디","새드앤딩"];
+  var 세얼간이=["코미디","교훈","청춘","드라마","음악","메세지"];
+  var 아수라=["한국","액션","범죄","느와르",];
+  var 범죄와의전쟁=["역사","다큐","범죄","느와르","액션","코미디","교훈"];
   var 가장보통의연애=["한국","코미디","성인","멜로","드라마"];
-  var 완벽한타인=["한국","코미디","교훈","철학","성인","드라마","가족"];
-  var 너의결혼식=["한국","멜로","교훈","드라마"];
-  var 캐치미이프유캔=["역사","코미디","교훈","철학","범죄","반전"];
-  var 테넷=["sf","액션","교훈","철학","범죄","판타지","반전"];
-  var 콰이엇플레이스=["스릴러","공포","액션","판타지"];
-  var 분노의질주=["액션","코미디","범죄","느와르","시리즈"];
-  var 테이큰=["액션","범죄","느와르"];
+  var 완벽한타인=["코미디","교훈","블랙코미디","성인","드라마","가족","메세지"];
+  var 너의결혼식=["한국","멜로","새드앤딩","드라마","청춘"];
+  var 캐치미이프유캔=["역사","코미디","교훈","메세지","범죄","반전"];
+  var 테넷=["sf","액션","서스팬스","철학","메세지","판타지","반전"];
+  var 콰이엇플레이스=["스릴러","공포","액션","괴물","서스팬스"];
+  var 분노의질주=["액션","코미디","범죄","느와르","스포츠","블록버스터"];
+  var 테이큰=["액션","범죄","느와르","가족","서스팬스"];
   var 아저씨=["액션","범죄","느와르","한국"];
   var 회사원=["액션","범죄","느와르","한국"];
-  var 해피데스데이=["하이틴","공포","스릴러","시리즈","성인","멜로"];
-  var 숨바꼭질=["공포","스릴러","한국","가족"];
-  var 승리호=["sf","판타지","한국","코미디","액션"];
-  var 판도라=["재난","한국","교훈"];
-  var 감기=["재난","한국","교훈"];
-  var 연가시=["재난","한국","교훈"];
-  var 부산행=["재난","한국","액션","괴물","스릴러","공포"];
-  var 월드워z=["재난","괴물","스릴러","공포","액션","sf"];
+  var 해피데스데이=["하이틴","공포","스릴러","코미디","성인","멜로"];
+  var 숨바꼭질=["스릴러","한국","가족","서스팬스"];
+  var 승리호=["sf","판타지","한국","코미디","액션","블록버스터"];
+  var 판도라=["재난","한국","교훈","가족","다큐"];
+  var 감기=["재난","한국","교훈","다큐","가족","서스팬스"];
+  var 연가시=["재난","한국","교훈","가족","서스팬스"];
+  var 부산행=["서스팬스","한국","액션","괴물","스릴러","가족"];
+  var 월드워z=["서스팬스","괴물","스릴러","블록버스터","액션","sf"];
   var 세벽의저주=["괴물","스릴러","공포","액션"];
-  var 마녀=["한국","액션","판타지","시리즈","범죄"];
-  var 맨인블랙=["액션","sf","판타지","시리즈","코미디"];
-  var 어거스트러쉬=["음악","드라마","교훈","철학"];
+  var 마녀=["한국","액션","판타지","서스팬스","범죄","가족"];
+  var 맨인블랙=["액션","sf","판타지","시리즈","코미디","블록버스터"];
+  var 어거스트러쉬=["뮤직","드라마","교훈","철학","가족"];
+
 
   var interest=["hi"];
   var i,j=0;
-  var result=[];
+var resolt = [];
+
+
+
+
+function add_img(name) {
+  if(name==="가장보통의연애"){
+  var img = document.createElement('img');
+    img.src = '가장보통의연애.png';}
+  if(name==="감기"){
+	var img = document.createElement('img');
+    img.src = '감기.png';}
+if(name==="기생충"){
+    var img = document.createElement('img');
+      img.src = '기생충.png';}
+if(name==="날씨의아이"){
+      var img = document.createElement('img');
+        img.src = '날씨의아이.png';}
+if(name==="내부자들"){
+        var img = document.createElement('img');
+          img.src = '내부자들.png';}
+if(name==="너의결혼식"){
+        	var img = document.createElement('img');
+            img.src = '너의결혼식.png';}
+if(name==="닥터스트레인지"){
+          	var img = document.createElement('img');
+              img.src = '닥터스트레인지.png';}
+if(name==="라라랜드"){
+            	var img = document.createElement('img');
+                img.src = '라라랜드.png';}
+if(name==="랑종"){
+              	var img = document.createElement('img');
+                  img.src = '랑종.png';}
+if(name==="마녀"){
+                	var img = document.createElement('img');
+                    img.src = '마녀.png';}
+if(name==="맨인블랙"){
+                  	var img = document.createElement('img');
+                      img.src = '맨인블랙.png';}
+if(name==="메이즈러너"){
+                    	var img = document.createElement('img');
+                        img.src = '메이즈러너.png';}
+if(name==="버드박스"){
+var img = document.createElement('img');
+img.src = '버드박스.png';}
+if(name==="범죄도시"){
+var img = document.createElement('img');
+img.src = '범죄도시.png';}
+if(name==="범죄와의전쟁"){
+var img = document.createElement('img');
+img.src = '범죄와의전쟁.png';}
+if(name==="분노의질주"){
+  var img = document.createElement('img');
+img.src = '분노의질주.png';}
+if(name==="세벽의저주"){
+var img = document.createElement('img');
+img.src = '세벽의저주.png';}
+if(name==="세얼간이"){
+var img = document.createElement('img');
+img.src = '세얼간이.png';}
+if(name==="숨바꼭질"){
+var img = document.createElement('img');
+img.src = '숨바꼭질.png';}
+if(name==="스타이즈본"){
+	var img = document.createElement('img');
+  img.src = '스타이즈본.png';}
+if(name==="승리호"){
+var img = document.createElement('img');
+img.src = '승리호.png';}
+if(name==="신세계"){
+var img = document.createElement('img');
+img.src = '신세계.png';}
+if(name==="아수라"){
+var img = document.createElement('img');
+img.src = '아수라.png';}
+if(name==="아이언맨"){
+  var img = document.createElement('img');
+img.src = '아이언맨.png';}
+if(name==="아저씨"){
+var img = document.createElement('img');
+img.src = '아저씨.png';}
+if(name==="액시트"){
+var img = document.createElement('img');
+img.src = '액시트.png';}
+if(name==="어거스트러쉬"){
+var img = document.createElement('img');
+img.src = '어거스트러쉬.png';}
+if(name==="어바웃타임"){
+	var img = document.createElement('img');
+  img.src = '어바웃타임.png';}
+if(name==="어벤져스"){
+var img = document.createElement('img');
+img.src = '어벤져스.png';}
+if(name==="연가시"){
+var img = document.createElement('img');
+img.src = '연가시.png';}
+if(name==="오백일의써머"){
+var img = document.createElement('img');
+img.src = '오백일의써머.png';}
+if(name==="완벽한타인"){
+  var img = document.createElement('img');
+img.src = '완벽한타인.png';}
+if(name==="위대한쇼맨"){
+var img = document.createElement('img');
+img.src = '위대한쇼맨.png';}
+if(name==="인셉션"){
+var img = document.createElement('img');
+img.src = '인셉션.png';}
+if(name==="인터스텔라"){
+var img = document.createElement('img');
+img.src = '인터스텔라.png';}
+if(name==="쥬만지"){
+	var img = document.createElement('img');
+  img.src = '쥬만지.png';}
+if(name==="칠번방의선물"){
+var img = document.createElement('img');
+img.src = '칠번방의선물.png';}
+if(name==="캐치미이프유캔"){
+	var img = document.createElement('img');
+  img.src = '캐치미이프유캔.png';}
+  if(name==="캡틴아메리카"){
+  	var img = document.createElement('img');
+    img.src = '캡틴아메리카.png';}
+if(name==="콰이엇플레이스"){
+var img = document.createElement('img');
+img.src = '콰이엇플레이스.png';}
+if(name==="테넷"){
+var img = document.createElement('img');
+img.src = '테넷.png';}
+if(name==="테이큰"){
+var img = document.createElement('img');
+img.src = '테이큰.png';}
+if(name==="판도라"){
+  var img = document.createElement('img');
+img.src = '판도라.png';}
+if(name==="해리포터"){
+var img = document.createElement('img');
+img.src = '해리포터.png';}
+if(name==="해피데스데이"){
+var img = document.createElement('img');
+img.src = '해피데스데이.png';}
+if(name==="회사원"){
+var img = document.createElement('img');
+img.src = '회사원.png';}
+document.getElementById('contents').appendChild(img);
+}
+
+
+
   function countarray(array){
      const cunts = array.reduce((pv, cv)=>{ pv[cv] = (pv[cv] || 0) + 1; return pv; }, {});
-     const resolt = []; for (let key in cunts) { resolt.push([key, cunts[key]]); }
+      for (let key in cunts) { resolt.push([key, cunts[key]]); }
     resolt.sort((first, second) => {return second[1] - first[1]; });
-    var s=0;
- while(s<11){
-document.write(resolt[s]+"점<br>");
-s=s+1;
-}}
+}
 
 
+function finish(sack){
+$('#목록').css('display',sack);
+}
 
-
-
-  var recognize={
+var recognize={
     saveinterest:function(name){
       i=0;
     while(i<name.length){
@@ -241,158 +387,160 @@ i=i+1;};
 
 },
   selectmovie:function(array){
-     const counts = array.reduce((pv, cv)=>{ pv[cv] = (pv[cv] || 0) + 1; return pv; }, {});
-     const result = []; for (let key in counts) {
-       if(액시트.includes(key)===true){
+    var o=0;
+     var result = [];
+     while(o<array.length) {
+       if(액시트.includes(array[o])===true){
        result.push("액시트");
      }
-       if(날씨의아이.includes(key)===true){
+       if(날씨의아이.includes(array[o])===true){
        result.push("날씨의아이");
      }
-       if(스타이즈본.includes(key)===true){
+       if(스타이즈본.includes(array[o])===true){
        result.push("스타이즈본");
        }
-       if(주만지.includes(key)===true){
+       if(주만지.includes(array[o])===true){
        result.push("주만지");
        }
-       if(라라랜드.includes(key)===true){
+       if(라라랜드.includes(array[o])===true){
        result.push("라라랜드");
        }
-       if(위대한쇼맨.includes(key)===true){
+       if(위대한쇼맨.includes(array[o])===true){
        result.push("위대한쇼맨");
        }
-       if(범죄도시.includes(key)===true){
+       if(범죄도시.includes(array[o])===true){
        result.push("범죄도시");
        }
-       if(인셉션.includes(key)===true){
+       if(인셉션.includes(array[o])===true){
        result.push("인셉션");
        }
 
-       if(인터스텔라.includes(key)===true){
+       if(인터스텔라.includes(array[o])===true){
        result.push("인터스텔라");
        }
-       if(기생충.includes(key)===true){
+       if(기생충.includes(array[o])===true){
        result.push("기생충");
        }
-       if(오백일의써머.includes(key)===true){
+       if(오백일의써머.includes(array[o])===true){
        result.push("오백일의써머");
        }
-       if(어바웃타임.includes(key)===true){
+       if(어바웃타임.includes(array[o])===true){
        result.push("어바웃타임");
        }
-       if(내부자들.includes(key)===true){
+       if(내부자들.includes(array[o])===true){
        result.push("내부자들");
        }
-       if(신세계.includes(key)===true){
+       if(신세계.includes(array[o])===true){
        result.push("신세계");
        }
-       if(해리포터.includes(key)===true){
+       if(해리포터.includes(array[o])===true){
        result.push("해리포터");
        }
-       if(아이언맨.includes(key)===true){
+       if(아이언맨.includes(array[o])===true){
        result.push("아이언맨");
        }
-       if(캡틴아메리카.includes(key)===true){
+       if(캡틴아메리카.includes(array[o])===true){
        result.push("캡틴아메리카");
        }
-       if(어벤져스.includes(key)===true){
+       if(어벤져스.includes(array[o])===true){
        result.push("어벤져스");
        }
-       if(토르.includes(key)===true){
+       if(토르.includes(array[o])===true){
        result.push("토르");
        }
-       if(닥터스트레인지.includes(key)===true){
+       if(닥터스트레인지.includes(array[o])===true){
        result.push("닥터스트레인지");
        }
-       if(메이즈러너.includes(key)===true){
+       if(메이즈러너.includes(array[o])===true){
        result.push("메이즈러너");
        }
-       if(랑종.includes(key)===true){
+       if(랑종.includes(array[o])===true){
        result.push("랑종");
        }
-       if(버드박스.includes(key)===true){
+       if(버드박스.includes(array[o])===true){
        result.push("버드박스");
        }
-       if(칠번방의선물.includes(key)===true){
+       if(칠번방의선물.includes(array[o])===true){
        result.push("칠번방의선물");
        }
-       if(세얼간이.includes(key)===true){
+       if(세얼간이.includes(array[o])===true){
        result.push("세얼간이");
        }
-       if(아수라.includes(key)===true){
+       if(아수라.includes(array[o])===true){
        result.push("아수라");
        }
-       if(범죄와의전쟁.includes(key)===true){
+       if(범죄와의전쟁.includes(array[o])===true){
        result.push("범죄와의전쟁");
        }
-       if(가장보통의연애.includes(key)===true){
+       if(가장보통의연애.includes(array[o])===true){
        result.push("가장보통의연애");
        }
-       if(완벽한타인.includes(key)===true){
+       if(완벽한타인.includes(array[o])===true){
        result.push("완벽한타인");
        }
-       if(너의결혼식.includes(key)===true){
+       if(너의결혼식.includes(array[o])===true){
        result.push("너의결혼식");
        }
-       if(캐치미이프유캔.includes(key)===true){
+       if(캐치미이프유캔.includes(array[o])===true){
        result.push("캐치미이프유캔");
        }
-       if(테넷.includes(key)===true){
+       if(테넷.includes(array[o])===true){
        result.push("테넷");
        }
-       if(콰이엇플레이스.includes(key)===true){
+       if(콰이엇플레이스.includes(array[o])===true){
        result.push("콰이엇플레이스");
        }
-       if(분노의질주.includes(key)===true){
+       if(분노의질주.includes(array[o])===true){
        result.push("분노의질주");
        }
-       if(테이큰.includes(key)===true){
+       if(테이큰.includes(array[o])===true){
        result.push("테이큰");
        }
-       if(아저씨.includes(key)===true){
+       if(아저씨.includes(array[o])===true){
        result.push("아저씨");
        }
-       if(회사원.includes(key)===true){
+       if(회사원.includes(array[o])===true){
        result.push("회사원");
        }
 
-       if(해피데스데이.includes(key)===true){
+       if(해피데스데이.includes(array[o])===true){
        result.push("해피데스데이");
        }
-       if(숨바꼭질.includes(key)===true){
+       if(숨바꼭질.includes(array[o])===true){
        result.push("숨바꼭질");
        }
-       if(승리호.includes(key)===true){
+       if(승리호.includes(array[o])===true){
        result.push("승리호");
        }
-       if(판도라.includes(key)===true){
+       if(판도라.includes(array[o])===true){
        result.push("판도라");
        }
-       if(감기.includes(key)===true){
+       if(감기.includes(array[o])===true){
        result.push("감기");
        }
-       if(연가시.includes(key)===true){
+       if(연가시.includes(array[o])===true){
        result.push("연가시");
        }
-       if(부산행.includes(key)===true){
+       if(부산행.includes(array[o])===true){
        result.push("부산행");
        }
-       if(월드워z.includes(key)===true){
+       if(월드워z.includes(array[o])===true){
        result.push("월드워z");
        }
-       if(세벽의저주.includes(key)===true){
+       if(세벽의저주.includes(array[o])===true){
        result.push("세벽의저주");
        }
-       if(어거스트러쉬.includes(key)===true){
+       if(어거스트러쉬.includes(array[o])===true){
        result.push("어거스트러쉬");
        }
-       if(마녀.includes(key)===true){
+       if(마녀.includes(array[o])===true){
        result.push("마녀");
        }
-       if(맨인블랙.includes(key)===true){
+       if(맨인블랙.includes(array[o])===true){
        result.push("맨인블랙");
        }
-      }
+       o=o+1;
+     };
 countarray(result);
   }
-  }
+}
